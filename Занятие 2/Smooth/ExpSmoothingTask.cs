05fc86f0aa3010e4a87bc82201eae5c6341dafd4
s7v7nn <narmincik989@gmail.com>
2025-02-25 20:49:42 +0300

    ibragimova lesson 2: expsmoothingtask

--- Занятие 2/Smooth/ExpSmoothingTask.cs
+++ Занятие 2/Smooth/ExpSmoothingTask.cs
@@ -1,12 +1,33 @@
 using System.Collections.Generic;
 
-namespace yield;
-
-public static class ExpSmoothingTask
+namespace yield
 {
-	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
-	{
-		//Fix me!
-		return data;
-	}
+    public static class ExpSmoothingTask
+    {
+        public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
+        {
+            var value = 0.0;
+            var isCheked = false;
+
+            foreach (var point in data)
+            {
+                var item = new DataPoint(point);
+
+                if (isCheked)
+                {
+                    item = point.WithExpSmoothedY(value + alpha * (point.OriginalY - value));
+                }
+
+                else
+                {
+                    item = point.WithExpSmoothedY(point.OriginalY);
+                    isCheked = true;
+                }
+
+                yield return item;
+                value = item.ExpSmoothedY;
+
+            }
+        }
+    }
 }
